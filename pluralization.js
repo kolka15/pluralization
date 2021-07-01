db4907461b87072b29608c7394cf3652ceb4ca23
Dmitriy Kozhushko <dvkozhushko@sberbank.ru>
2021-07-01 17:44:56 +0300

    es6

--- pluralization.js
+++ pluralization.js
@@ -1,12 +1,10 @@
 /**
-*
-* use: declOfNum(count, ['материал', 'материала', 'материалов']);
-*
-* */
+ *
+ * use: declOfNum(count, ['материал', 'материала', 'материалов']);
+ *
+ * */
 
-function declOfNum(number, titles) {
-    let cases = [2, 0, 1, 1, 1, 2];
-    return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];
+export const declOfNum = (number, titles) => {
+    const cases = [2, 0, 1, 1, 1, 2];
+    return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
 }
-
-module.exports = declOfNum
